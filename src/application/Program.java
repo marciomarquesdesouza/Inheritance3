70ac4c8db2d8a3c530e7a1f1ec8a14bf77ea0ea0
Marcio Marques de Souza <marciomarques.contato@hotmail.com>
2020-03-27 10:58:55 -0300

    Alteracao de nome do proprietario

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
+		Account acc1 = new Account(1001, "Marcio", 1000.0);
 		acc1.withdraw(200.0);
 		System.out.println(acc1.getBalance());
 		
