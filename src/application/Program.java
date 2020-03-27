4b022a970566347ff6f872859e0dd4c834288e15
Marcio Marques de Souza <marciomarques.contato@hotmail.com>
2020-03-27 15:20:51 -0300

    Implementacao de classe abstrata

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingAccount;
@@ -8,16 +13,26 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Marcio", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		Locale.setDefault(Locale.US);
+		List<Account> list = new ArrayList<>();
 		
-		Account acc2 = new SavingAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		list.add(new SavingAccount(1001, "Maria", 500.0, 0.01));
+		list.add(new BusinessAccount(1002, "Morgan", 1000.0, 400.0));
+		list.add(new SavingAccount(1003, "Marcio", 300.0, 0.01));
+		list.add(new BusinessAccount(1004, "Miriam", 500.0, 500.0));
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		double sum = 0.0;
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for (Account acc : list) {
+			acc.deposit(10.0);
+		}
+		for (Account acc : list) {
+			System.out.printf("Update balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
 }
